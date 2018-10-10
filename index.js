376294f2d8a242e2393c667eb892846667a51759
rw400 <rwong400@gmail.com>
2018-10-10 16:30:26 -0400

    updated fetch request javascript for eth price

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
-const API_URL = "https://api.etherscan.io/api?module=account&action=balance&address=0xddbd2b932c763ba5b1b7ae3b362eac3e8d40121a&tag=latest&apikey="
+const API_URL_PRICE = "https://api.etherscan.io/api?module=stats&action=ethprice&apikey="
 const TOKEN = "EK576RXRMNFN9VT6P48UHHDJWIJSP15GS3"
 const FULL_URL = API_URL + TOKEN
 
-//Class example
-function request() {
-    fetch(FULL_URL)
-    .then(function(result){
-        return result.json();
-    })
-    .then(function(res){
-        console.log(res)
-        
-        //throw error
-        // if(res.status >== 400){
-        //     throw ;
-        // }else{
-        //     nextFun(res)
-        // }
-    })
-    .catch(function(error){
-        console.log(error)
+// fetch ether price
+function Price() {
+    window.addEventListener("load", () => {
+        fetch(API_URL_PRICE)
+            .then(function(result) {
+
+                if (res.status == 200) {        // check status code
+                    let pricestatus = result.json()
+                    pricestatus.then(function(res){
+                        //get and display price in header
+                        let etherprice = document.querySelector('#ethprice')
+                        currentprice.innerHTML += '' + res.result.ethbtc
+                    })
+                }else {
+                   console.log(res)            //error in status code
+                }
+            
+            })
+        .catch(function(error) {
+            console.log(error)         //error in the network
+        })
     })
 }
